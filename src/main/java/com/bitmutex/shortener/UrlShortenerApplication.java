14c59e621ccb309e92e7ffd7fd155c6727850c9b
Amit Kumar Nandi <11887616+aamitn@users.noreply.github.com>
2024-02-20 02:59:08 +0530

    final pom changes

--- src/main/java/com/bitmutex/shortener/UrlShortenerApplication.java
+++ src/main/java/com/bitmutex/shortener/UrlShortenerApplication.java
@@ -1,3 +1,18 @@
+/**
+ * @file  gateway.php
+ * @brief Url Shortener Application
+ *         Github Repository : https://github.com/aamitn/URLShortener
+ * JAVA 21 / Spring Boot 3
+ *
+ * @author    Amit Kumar Nandi (Bitmutex Technologies) <amit@bitmutex.com>
+ * @version   1.0.5
+ * @date      2023-03-30
+ * @since     2022-09-10
+ * @copyright (c) 2023-2024 Bitmutex Technologies
+ * @copyright GNU Lesser General Public License
+ *
+ *********************************************************************/
+
 package com.bitmutex.shortener;
 
 import org.springframework.boot.SpringApplication;
